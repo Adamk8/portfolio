a1f84b38cc130873b49c37516e9af74fd71b0dc3
Adam Kwan <adam876k@gmail.com>
2022-06-19 18:21:42 -0700

    Setting up github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
-import {Route, Routes} from 'react-router-dom'
-import { Home } from "./pages/Home"
-
-
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Home />} />
     </Routes>
   </ChakraProvider>
-)
+);
